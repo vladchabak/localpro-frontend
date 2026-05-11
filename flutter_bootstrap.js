d029c2d0784acea038282b3d6618aae8094283a9
Vlad Chabak <jv.vlad.chabak@gmail.com>
2026-05-11 21:54:15 +0300

    fix: null safety in booking request creation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1525754265" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2717224986" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
